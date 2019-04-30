2589531b8c001b361644acbde92089a9af7e26ae
Your Namevibhormittal214 <vibhormittal214.com>
2019-04-30 10:25:10 +0530

    First commit

---
+++ CreateCSV.py
@@ -0,0 +1,20 @@
+from scipy.misc import imread
+import numpy as np
+import pandas as pd
+import os
+root = './gestures' 
+
+
+for directory, subdirectories, files in os.walk(root):
+
+    for file in files:
+   
+        print(file)
+        im = imread(os.path.join(directory,file))
+        value = im.flatten()
+
+        value = np.hstack((directory[8:],value))
+        df = pd.DataFrame(value).T
+        df = df.sample(frac=1) # shuffle the dataset
+        with open('train_foo.csv', 'a') as dataset:
+            df.to_csv(dataset, header=False, index=False)
